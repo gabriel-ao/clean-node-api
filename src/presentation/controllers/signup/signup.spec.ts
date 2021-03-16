77b4d28cf034f31604b73fda465ee7554360db3f
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2021-03-16 17:54:02 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -224,4 +224,25 @@ describe('SignUp Controller', () => {
       password: 'any_passoword',
     });
   });
+
+  test('Should return 200 if an valid  data is provided', () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_passoword',
+        passwordConfirmation: 'valid_passoword',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    });
+  });
 });
